07f14e8094c36b44751c3e64466f9715c55cc79f
林展丞 <105207346@mail2.nccu.tw>
2020-10-30 23:07:22 +0100

    this is mine first function

--- DrawingGraph.py
+++ DrawingGraph.py
@@ -5,30 +5,30 @@ from datetime import datetime    #library to work with time data
 #loading the CSV file and peek what is inside the Pandora's Box
 df = pd.read_csv("CleanedDataInNoteBook.csv") 
 df.info()
-df.head(5)
+#df.head(5)
 
 #convert date from string into datatime(a data type in panda) 
 df["date"] = pd.to_datetime(df["date"], format="%Y%m%d")
 #check if I fuck up or not 
 df.info()
 
-#convert date from string into datatime(a data type in panda) 
-df["date"] = pd.to_datetime(df["date"], format="%Y%m%d")
-#check if I fuck up or not 
-df.info()
-
+df=df.groupby(['date']).sum().reset_index()
 #prepare the data for graph
 showdf=df[['date','in','out']]
+showdf.info()
 
 #do some filter because the data is to many to show in one graph
-start_date = "2017-1-1"
-end_date = "2017-12-31"
-
+start_date = "2019-1-1"
+end_date = "2019-12-31"
+#print(showdf)
 after_start_date = showdf["date"] >= start_date
 before_end_date = showdf["date"] <= end_date
 between_two_dates = after_start_date & before_end_date
 showdf = df.loc[between_two_dates]
 
+#print(showdf)
+
 #plz work, just show me the graph
-showdf.plot.line(x="date", y=["in", "out"],figsize=(20,5))
+fig=showdf.plot.line(x="date", y=["in", "out"],figsize=(10,5))
+plt.savefig('19chart.png')
 plt.show()
